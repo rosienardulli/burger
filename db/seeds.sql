b7af678c0fcf845f1a9a90b9d4421022330dad48
Rosie Nardulli <rosie.nardulli@gmail.com>
2018-05-24 21:45:33 -0500

    burger

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheese Burger');
